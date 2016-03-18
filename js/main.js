6ee8421c701b9070f5f102fd764748c337db682e
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-17 23:25:19 -0400

    Applied content

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ var $belowveggies = $('<ul>');
 var $aboveH1 = $('<h1>');
 var $aboveveggies = $('<ul>');
 
+$oneH1.html('Below ground veggies');
+$('body').prepend($oneH1);
+
+$('body').append($belowveggies);
+$aboveH1.html('Above ground veggies');
+$('body').append($aboveH1);
+
+$('body').append($aboveveggies);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $belowveggies.append($(this));
+  } else {
+    $aboveveggies.append($(this));
+  }
+});
